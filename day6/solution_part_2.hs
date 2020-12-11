2765c5c673bbcec4533c63485475a7752fe495a3
Landon Gilbert-Bland <lbland@instructure.com>
2020-12-11 10:57:32 -0700

    Fold1 lets us cleanly one line this one too. Neat!

--- day6/solution_part_2.hs
+++ day6/solution_part_2.hs
@@ -1,13 +1,6 @@
 import Data.List
 import Data.List.Split
-import Data.Function
 import System.IO
 
-numAllAnsweredYes :: [String] -> Int
-numAllAnsweredYes (x:xs) = length $ foldr (\x acc -> intersect x acc) x xs
-
-main  = do
-  fh <- openFile "input.txt" ReadMode
-  input <- hGetContents fh
-  print . sum . map numAllAnsweredYes . map words . splitOn "\n\n" $ input
-  hClose fh
+main =
+  print . sum . map length . map (foldr1 intersect) . map words . splitOn "\n\n"  =<< hGetContents =<< openFile "input.txt" ReadMode
